6e1b9e751a8ff6abbeb91c12d504c8b1ffddd00e
Joaquin <jearce@fundacionparaguaya.org.py>
2025-10-19 23:33:47 -0300

    conectar back de railway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  backendApi: 'http://localhost:3000/gpt'
+  production: true,
+  backendApi: 'https://tesis-fonokids-joaquin-backend-ia-production.up.railway.app/gpt',
+  backendLogin: 'https://tesis-fonokids-joaquin-backend-login-production.up.railway.app'
 };
