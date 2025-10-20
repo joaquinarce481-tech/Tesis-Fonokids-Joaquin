3ceb5f5a261839c6ee7ce63c26b4c46b05786ebf
Joaquin <jearce@fundacionparaguaya.org.py>
2025-10-19 23:36:23 -0300

    conectar back con ts.produ creado

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  backendApi: 'https://tesis-fonokids-joaquin-backend-ia-production.up.railway.app/gpt',
+  backendLogin: 'https://tesis-fonokids-joaquin-backend-login-production.up.railway.app'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: true,
-  backendApi: 'https://tesis-fonokids-joaquin-backend-ia-production.up.railway.app/gpt',
-  backendLogin: 'https://tesis-fonokids-joaquin-backend-login-production.up.railway.app'
+  backendApi: 'http://localhost:3000/gpt'
 };
