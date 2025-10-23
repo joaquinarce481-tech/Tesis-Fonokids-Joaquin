fc01e6f51a5b395c7b49ef6a146a1ab8e9c0389a
Joaquin <jearce@fundacionparaguaya.org.py>
2025-10-23 18:16:17 -0300

    Implementar proxy serverless en Vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendApi: '/api',       // ✅ Usa el proxy de Vercel
-  backendLogin: '/api'      // ✅ Usa el proxy de Vercel
+  backendApi: '/api',
+  backendLogin: '/api'
 };
